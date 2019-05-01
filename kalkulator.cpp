7941ab9d66fdf56173af9ab1a4488bfa89298ff3
Robert <latoszewskirobert@gmail.com>
2019-05-01 13:54:42 +0200

    Pierwszy commit

---
+++ kalkulator.cpp
@@ -0,0 +1,75 @@
+/*
+    Autor: Robert Latoszewski
+    Data utworzenia: 01.05.2019
+
+    Opis: 
+        Kalkulator macierzowy
+*/
+
+#include <iostream>
+
+using namespace std;
+
+//*****************************************************************************
+//****** deklaracje funkcji ******//
+
+void wczytaj_dane(int i);
+
+//*****************************************************************************
+//****** glowny program ********// 
+
+int main()
+{
+    system("clear");
+    cout << "*** Kalkulator macierzowy ***\n\n";
+    cout << "Wybierz operacje:\n"
+            "1. Dodawanie\n"
+            "2. Odejmowanie\n"
+            "3. Mnozenie\n"
+            "4. Mnozenie przez liczbe\n"
+            "5. Wyjscie\n";
+    int operacja;
+    cin >> operacja;
+    switch (operacja)
+    {
+        case 1:     //dodawanie
+            cout << "przypadek 1\n";
+            break;
+        case 2:     //odejmowanie
+            cout << "przypadek 2\n";
+            break;
+        case 3:     //mnozenie
+            cout << "przypadek 3\n";
+            break;
+        case 4:     //mnozenie przez liczbe
+            cout << "przypadek 4\n";
+            break;
+        case 5:     //wyjscie
+            cout << "przypadek 5\n";
+            break;
+        default:
+            cout << "Bledne dane, koncze program\n";
+            return 0;
+    }
+    return 0;
+}
+
+//*****************************************************************************
+//****** definicje funkcji ********// 
+
+void wczytaj_dane(int i)
+{
+    int wiersz_1, wiersz_2, kolumna_1, kolumna_2;
+    if (i)
+    {
+        cout << "Rozmiar 1 matrycy: ";
+        cin >> wiersz_1 >> kolumna_1;
+        cout << "\nRozmiar 2 matrycy: ";
+        cin >> wiersz_2 >> kolumna_2;
+        if (i == 1 || i == 2)
+        {
+            if (wiersz_1 != wiersz_2 || kolumna_1 != kolumna_2)
+            cout << "Operacja niewykonalna\n";
+        }
+    }
+}
